f704bbcb6fe0d92d9474c813d58f2161b8f77419
Afif718 <afifahmad718@gmail.com>
2023-03-20 04:11:21 +0800

    The first Commit

---
+++ Java-Multithread-Chat-Application/src/main/java/multithreadChatApplication/Java/Multithread/Chat/Application/JavaMultithreadChatApplication.java
@@ -0,0 +1,13 @@
+package multithreadChatApplication.Java.Multithread.Chat.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMultithreadChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMultithreadChatApplication.class, args);
+	}
+
+}
---
+++ Java-Multithread-Chat-Application/src/test/java/multithreadChatApplication/Java/Multithread/Chat/Application/JavaMultithreadChatApplicationTests.java
@@ -0,0 +1,13 @@
+package multithreadChatApplication.Java.Multithread.Chat.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMultithreadChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
